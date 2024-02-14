8abb2e4e2f4bd8694d2322a32e6dbe8d0fe33241
Da Silva Clement <clementdasilva0@gmail.com>
2024-02-14 17:53:25 +0100

    ajout db-config

---
+++ config/db-config.php
@@ -0,0 +1,9 @@
+<?php
+//Définir les informations de connexion
+const DB_HOST = "localhost:3306";
+const DB_NAME = "db_cinema";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+
+//Utiliser PDO pour créer une connexion à la DB
+$pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
